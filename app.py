b61e94204b9fc0f3dd76dd21d0008fa7b425d5ad
aimanalishezan <aimanalishezan22@gmail.com>
2025-04-06 23:56:27 +0600

    update

---
+++ app.py
@@ -0,0 +1,19 @@
+import os 
+from dotenv import load_env
+import ollama
+import gradio as gr
+
+load_env()
+
+model=os.getenv('mod')
+
+
+MODEL=model
+
+
+system_message="you are a helpful assistant for an airline called flighAI"
+system_message+="give a short, courteous answer, no more then 1 sentence."
+system_message+="Always be accurate . if you don't know the answer , say so ."
+
+#function for the tool
+ticket_price={"dhaka":"10000tK","thakurgaon":"5000tk","sylet":"7000tk","morocco":"40000"}
